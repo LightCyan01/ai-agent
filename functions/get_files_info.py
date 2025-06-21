6d3e066b0b12fac995c3888be55b81df76b1d504
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-21 02:39:46 -0400

    Remove unused functions and constants from get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,5 @@
 import os
 
-MAX_CHARS = 10000
-
 def get_files_info(working_directory, directory=None):
     wd_abspath = os.path.abspath(working_directory)
     
@@ -22,46 +20,3 @@ def get_files_info(working_directory, directory=None):
         is_dir = os.path.isdir(file_path)
         file_list.append(f"- {file}: file_size={size} bytes, is_dir={is_dir}")
     return "\n".join(file_list)
-    
-
-def get_file_content(working_directory, file_path):
-    wd_abspath = os.path.abspath(working_directory)
-    
-    if file_path:
-        target_file = os.path.abspath(os.path.join(wd_abspath, file_path))
-    
-    if not target_file.startswith(wd_abspath):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.isfile(target_file):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    with open(target_file, "r") as f:
-        content = f.read()
-        
-        if len(content) > MAX_CHARS:
-            trunc = content[:MAX_CHARS] + f'[...File "{target_file}" truncated at 10000 characters]'
-            return trunc
-        
-        return content
-    
-
-def write_file(working_directory, file_path, content):
-    wd_abspath = os.path.abspath(working_directory)
-    
-    if file_path:
-        target_file = os.path.abspath(os.path.join(wd_abspath, file_path))
-    
-    if not target_file.startswith(wd_abspath):
-        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.exists(target_file):
-        try:
-            if not os.path.dirname(target_file):
-                os.makedirs(os.path.dirname(target_file))
-        except Exception as e:
-            return f'Error: {e}'
-    
-    with open(target_file, "w") as f:
-        f.write(content)
-        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
